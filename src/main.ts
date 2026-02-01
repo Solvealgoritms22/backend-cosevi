15c87bd214f1a5c32a7a53f6dcfcc326dab301e3
Darling Fajardo <darlingf1998@gmail.com>
2026-02-01 10:43:21 -0400

    feat: Initialize NestJS application with global validation, CORS, and body parser configurations.

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+
+import { json, urlencoded } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(json({ limit: '50mb' }));
+  app.use(urlencoded({ limit: '50mb', extended: true }));
 
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
